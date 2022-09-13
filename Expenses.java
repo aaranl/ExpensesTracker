b491bb49271893bf6e85a6347d947f3bde23a320
Aaran Lawing <lawingaaran@gmail.com>
2022-09-13 16:51:15 -0400

    Expenses Tracker that stores information

---
+++ Expenses.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import java.io.File;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Expenses {
+
+    public static void main (String[] args) {
+            
+
+        System.out.println("1. Income");
+        System.out.println("2. Gas");
+        System.out.println("3. Food");
+        System.out.println("4. Bills");
+        System.out.println("5. Other");
+        System.out.println("");
+        System.out.println("Input type of Expense");
+
+        //hello
+        
+
+    }
+}
